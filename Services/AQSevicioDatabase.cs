73967eb8790b3427d1d61f4369e5d15a39531920
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-07-10 11:23:42 -0500

    Creacion de AQSevicioDatabase y cambios en AQGamesGuardados, MainPage y AppShell por errores

---
+++ Services/AQSevicioDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Quezada_Evaluacion3P.Models;
+
+namespace Quezada_Evaluacion3P.Services
+{
+    public class AQSevicioDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public AQSevicioDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<AQGame>().Wait();
+        }
+
+        public Task<List<AQGame>> GetGamesAsync()
+        {
+            return _database.Table<AQGame>().ToListAsync();
+        }
+
+        public Task<int> SaveGameAsync(AQGame game)
+        {
+            return _database.InsertAsync(game);
+        }
+    }
+}
+
+
+
