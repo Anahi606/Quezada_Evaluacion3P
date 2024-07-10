57b1b6a3242db23c5e825bb80f24167a44516517
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-07-10 11:00:54 -0500

    Creacion View y modificaciones en AppShell y MainPage

---
+++ ViewModels/AQBaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quezada_Evaluacion3P.ViewModels
+{
+    public class AQBaseViewModel : INotifyPropertyChanged
+    {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                if (_isBusy != value)
+                {
+                    _isBusy = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/AQGamesGuardados.xaml.cs
@@ -0,0 +1,9 @@
+namespace Quezada_Evaluacion3P.Views;
+
+public partial class AQGamesGuardados : ContentPage
+{
+	public AQGamesGuardados()
+	{
+		InitializeComponent();
+	}
+}
